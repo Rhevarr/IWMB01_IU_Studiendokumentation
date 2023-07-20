4922c5941ef8a8fc02bccd766d440067fe2ef765
Rhevarr <rudi.eckes@gmail.com>
2023-07-20 14:19:46 +0200

    Portrait-Modus eingestellt. Kleinere Änderungen.

--- app/src/main/java/de/iu/iwmb01_iu_studiendokumentation/NewEditCourse.java
+++ app/src/main/java/de/iu/iwmb01_iu_studiendokumentation/NewEditCourse.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class NewCourse extends AppCompatActivity {
+public class NewEditCourse extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_course);
+        setContentView(R.layout.activity_new_edit_course);
     }
 }
