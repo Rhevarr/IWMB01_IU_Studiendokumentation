c81c5ee1a80cf4ff4031a40d1fd7eaf9e2933dc3
Rhevarr <rudi.eckes@gmail.com>
2023-07-20 12:09:22 +0200

    New_Course_Activity erstellt zum Erstellen neuer Kurse

---
+++ app/src/main/java/de/iu/iwmb01_iu_studiendokumentation/NewCourse.java
@@ -0,0 +1,14 @@
+package de.iu.iwmb01_iu_studiendokumentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewCourse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_course);
+    }
+}
