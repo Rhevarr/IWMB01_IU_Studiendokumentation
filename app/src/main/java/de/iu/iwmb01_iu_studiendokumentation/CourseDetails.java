a964ca08ad79d9705b703c2964eec5bdab8bf0e3
Rhevarr <rudi.eckes@gmail.com>
2023-07-20 17:07:41 +0200

    Activity zur Verwaltung der Lerneinheiten erstellt.

---
+++ app/src/main/java/de/iu/iwmb01_iu_studiendokumentation/CourseDetails.java
@@ -0,0 +1,14 @@
+package de.iu.iwmb01_iu_studiendokumentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CourseDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+    }
+}
