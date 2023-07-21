8f5cc184e15fe5aff2115166831b743fcbd16833
Rhevarr <rudi.eckes@gmail.com>
2023-07-21 13:49:09 +0200

    learning_unit_details erstellt

---
+++ app/src/main/java/de/iu/iwmb01_iu_studiendokumentation/LearningUnitDetails.java
@@ -0,0 +1,14 @@
+package de.iu.iwmb01_iu_studiendokumentation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LearningUnitDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_learning_unit_details);
+    }
+}
